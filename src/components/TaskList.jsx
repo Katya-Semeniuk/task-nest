1b4c7b8f71ab06dac4afdcdb5abfd0efc4072959
Kateryna Semeniuk <katyasemeniuk@gmail.com>
2025-01-06 18:53:33 +0000

    added stylisation to TaskList component

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import styles from "../styles/TaskList.module.css";
+import { TbPointFilled } from "react-icons/tb";
 
 
 function TaskList({ tasks }) {
@@ -8,31 +9,32 @@ function TaskList({ tasks }) {
 
   return (
     <Container>
-      <Row>
+     
         {tasks.map((task) => (
-          <Col key={task.id}>
+             <Row key={task.id} className={styles.Box}>
+          <Col >
             <Card>
               <Card.Body>
                 <div className="d-flex align-items-center justify-content-between">
-                  <Card.Title>{task.title}</Card.Title>
+                  <Card.Title className={styles.Title} >{task.title}</Card.Title>
                   <Card.Subtitle className="mb-2 text-muted">
                     {task.updated_at}
                   </Card.Subtitle>
                 </div>
 
-                <Card.Text>{task.description}</Card.Text>
-                <Card.Text> <span>Status:</span>  {task.status}</Card.Text>
-
-                <Card.Text>{task.comments_count}</Card.Text>
+                <Card.Text className="mb-3">
+                <span className="mr-2">Description:</span>{task.description}</Card.Text>
+                <Card.Text className="mb-2"> <TbPointFilled className={styles.Blue}/><span className="mr-2">Status:</span>  {task.status}</Card.Text>
+                <Card.Text className="mb-2"> <TbPointFilled className={styles.Blue}/><span className="mr-2">Category:</span>  {task.category}</Card.Text>
+                <Card.Text className="mb-2"> <TbPointFilled className={styles.Blue}/><span className="mr-2">Comments:</span>{task.comments_count}</Card.Text>
 
                 <Card.Text>
+                <TbPointFilled className={styles.Blue}/>
                   <span className="mr-2">Assigned_to:</span>
-                  {tasks.assigned_users && tasks.assigned_users.length > 0 ? (
-                    tasks.assigned_users.map((user) => (
-                      <span key={user.id}>{user.username}</span>
-                    ))
+                  {tasks.assigned_to && tasks.assigned_to > 0 ? (
+                    <span>{tasks.assigned_to} users</span>
                   ) : (
-                    <span >No users assigned.</span>
+                    <span >no users assigned</span>
                   )}
                 </Card.Text>
 
@@ -42,8 +44,9 @@ function TaskList({ tasks }) {
               </Card.Body>
             </Card>
           </Col>
+          </Row>
         ))}
-      </Row>
+      
     </Container>
   );
 }
